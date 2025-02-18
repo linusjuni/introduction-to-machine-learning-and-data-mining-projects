538651f835178cedf48e4187065941b34b767e87
linusjuni <s225224@student.dtu.dk>
2025-02-18 18:05:44 +0100

    imports fixed

--- 1_project/src/import_data.py
+++ 1_project/src/import_data.py
@@ -1,14 +1,11 @@
-import importlib_resources
-import matplotlib
-matplotlib.use('TkAgg')
 import matplotlib.pyplot as plt
 import numpy as np
-from scipy.io import loadmat
 import dtuimldmtools
 import pandas as pd
 import os
+from scipy.linalg import svd
 
-project_root = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))  # Go up two levels
+project_root = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 filename = os.path.join(project_root, "data", "Concrete_Data.xls")
 data = pd.read_excel(filename)
 print(data.head())
