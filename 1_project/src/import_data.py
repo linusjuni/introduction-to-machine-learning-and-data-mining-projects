a7eabbca91accf0acf8c234907a8ba54f4c16795
oscarwohlfahrt <oscarjhw@gmail.com>
2025-02-18 18:18:55 +0100

    main function

--- 1_project/src/import_data.py
+++ 1_project/src/import_data.py
@@ -5,8 +5,14 @@ import pandas as pd
 import os
 from scipy.linalg import svd
 
-project_root = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-filename = os.path.join(project_root, "data", "Concrete_Data.xls")
-data = pd.read_excel(filename)
-print(data.head())
+def extract_data():
+    project_root = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+    filename = os.path.join(project_root, "data", "Concrete_Data.xls")
+    data = pd.read_excel(filename)
+    print(data.head())
 
+def main():
+    extract_data()
+
+if __name__ == "__main__":
+    main()
