053be0e8c1367ecebdc22a28121546601fd8534f
oscarwohlfahrt <oscarjhw@gmail.com>
2025-04-03 19:25:09 +0200

    getting weights and offset

--- 2_project/src/one_layer_regularization.py
+++ 2_project/src/one_layer_regularization.py
@@ -8,7 +8,12 @@ from pathlib import Path
 import seaborn as sns
 from dtuimldmtools import rlr_validate
 from load_data import X, y
-lambdas = np.logspace(-2, 7, 50)
+
+X = np.concatenate((np.ones((X.shape[0], 1)), X), 1)
+N, M = X.shape
+
+lambdas = np.logspace(-2, 7, 100)
+w_rlr = np.empty(M)
 
 (opt_val_err,
 opt_lambda,
@@ -24,6 +29,9 @@ plt.xlabel("Regularization factor")
 plt.ylabel("Squared error (crossvalidation)")
 plt.legend(["Train error", "Validation error"])
 plt.grid()
-
 plt.show()
-print(test_err_vs_lambda)
+
+lambdaI = opt_lambda * np.eye(M)
+lambdaI[0, 0] = 0 
+w_rlr[:] = np.linalg.solve((X.T @ X) + lambdaI, X.T @ y).squeeze()
+
