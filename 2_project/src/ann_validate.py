f0d55740ee83a28dd34f445effbe35d0b101a7a6
oscarwohlfahrt <oscarjhw@gmail.com>
2025-04-05 14:15:09 +0200

    ann_validate function

---
+++ 2_project/src/ann_validate.py
@@ -0,0 +1,52 @@
+import torch
+import numpy as np
+from dtuimldmtools import draw_neural_net, train_neural_net
+from sklearn import model_selection
+
+def ann_validate(X, y, hs, cvf=10):
+    CV = model_selection.KFold(cvf, shuffle=True)
+    M = X.shape[1]
+    test_error = np.empty((cvf, len(hs)))
+    f = 0
+    y = y.squeeze()
+
+    for train_index, test_index in CV.split(X, y):
+        X_train = torch.Tensor(X[train_index, :])
+        y_train = torch.Tensor(y[train_index])
+        X_test = torch.Tensor(X[test_index, :])
+        y_test = torch.Tensor(y[test_index])
+
+        for l in range(0, len(hs)):
+            model = lambda: torch.nn.Sequential(
+            torch.nn.Linear(M, hs[l]),  # M features to n_hidden_units
+            torch.nn.Tanh(),  # 1st transfer function,
+            torch.nn.Linear(hs[l], 1),  # n_hidden_units to 1 output neuron
+            # no final tranfer function, i.e. "linear output")
+            )
+            loss_fn = torch.nn.MSELoss()
+            
+            net, final_loss, learning_curve = train_neural_net(
+            model,
+            loss_fn,
+            X=X_train,
+            y=y_train,
+            n_replicates=1,
+            max_iter=10000,)
+            
+            y_test_est = net(X_test)
+
+            se = (y_test_est.float() - y_test.float()) ** 2  # squared error
+            mse = (sum(se).type(torch.float) / len(y_test)).data.numpy()  # mean
+            test_error[f,l] = mse
+
+        f = f + 1
+
+    opt_val_err = np.min(np.mean(test_error, axis=0))
+    opt_h = hs[np.argmin(np.mean(test_error, axis=0))]
+    test_err_vs_h = np.mean(test_error, axis=0)
+
+    return (
+        opt_val_err,
+        opt_h,
+        test_err_vs_h
+    )
