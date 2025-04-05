da0ebbc1cf4d5573bd2a463c966fa3487af2b8d2
oscarwohlfahrt <oscarjhw@gmail.com>
2025-04-05 15:53:09 +0200

    start classification

---
+++ 2_project/src/classification.py
@@ -0,0 +1,25 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+import torch
+from scipy.linalg import svd
+from pathlib import Path
+import seaborn as sns
+from dtuimldmtools import rlr_validate
+from sklearn import model_selection
+from load_data import X_normalized, y
+from dtuimldmtools import draw_neural_net, train_neural_net
+from ann_validate import *
+from tqdm import tqdm  # For progress bars
+
+K = 5
+CV = model_selection.KFold(K, shuffle=True)
+
+Error_test_rlr = np.empty((K, 1))
+Error_test_baseline = np.empty((K, 1))
+opt_lamdas = np.empty([K,1])
+
+k = 0
+for train_index, test_index in tqdm(CV.split(X_normalized, y2),total = K, desc="Outer CV folds"):
+
+    k = k + 1
